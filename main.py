93e1f5a848bb5a829486d0d729dc88ea8d180153
fastpostjj <fastpost@yandex.ru>
2023-11-10 00:21:59 +0300

    added class Score

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ Code 4: Token Empty Session Token has returned all possible
 questions for the specified query. Resetting the Token is necessary.
 """
 
+
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -46,13 +47,10 @@ class Question:
 
     def print_question(self, index: int) -> dict:
         print(f"{self.question[index]['question']}")
-        # all_answers = self.question[index]['incorrect_answers']
-        # all_answers.append(self.question[index]['correct_answer'])
-        # random.shuffle(all_answers)
-        for index, answer in enumerate(self.question[index]['answers']):
-            print(f'{index + 1} - {answer}')
+        for i, answer in enumerate(self.question[index]['answers']):
+            print(f'{i + 1} - {answer}')
         print()
-        return self.question
+        return self.question[index]
 
     def transform_question(self, question: dict) -> dict:
         transformed_question = {}
@@ -67,43 +65,57 @@ class Question:
                 transformed_question['index_correct_answer'] = index
         return transformed_question
 
+    def is_correct_answer(self, question: dict, index: int) -> bool:
+        if 'index_correct_answer' in question and \
+                index == question['index_correct_answer']:
+            return True
+        return False
+
     def print_questions(self) -> None:
         for q in range(len(self.question)):
             self.print_question(q)
-        # print(f"{self.question[index]['question']}")
-            # self.print_answers(
-            #     self.question[q]['correct_answer'],
-            #     self.question[q]['incorrect_answers']
-            #     )
 
-    # def print_answers(
-    #         self,
-    #         correct_answer: str = None,
-    #         incorrect_answers: list[str] = []
-    #         ) -> str:
-    #     all_answers = incorrect_answers
-    #     all_answers.append(correct_answer)
-    #     random.shuffle(all_answers)
-    #     print(all_answers)
-    #     return all_answers
 
-    # def shuffle_questions(self):
-    #     random.shuffle(self.question)
+class Score:
+    def __init__(self):
+        self._point = 0
+        self._name = ""
 
-    # def __str__(self) -> str:
-    #     for q in range(len(self.question)):
-    #         self.print_question(q)
+    @property
+    def point(self) -> int:
+        return self._point
 
+    @point.setter
+    def point(self, point: int) -> int:
+        if not isinstance(point, int):
+            raise ValueError("Value point must be an integer!")
+        self._point = point
+        return self._point
+
+    def point_add(self, point: int) -> int:
+        self.point += point
+        return self.point
+
+    @property
+    def name(self) -> int:
+        return self._name
+
+    @name.setter
+    def name(self, name: int) -> int:
+        self._name = name
+        return self._name
+
+    def __str__(self) -> str:
+        return f'{self.name}: {self.point}'
 
 
 class Quiz:
     def get_questions(self) -> json:
-        url = f'https://opentdb.com/api.php?amount=10&category={self.category_number}&difficulty={self.difficutly}'
+        url = 'https://opentdb.com/api.php?amount=10&'\
+              + f'category={self.category_number}&difficulty={self.difficutly}'
         response = requests.get(url)
-        # print("response.status_code=", response.status_code)
         if response.status_code:
             if response.json()['response_code'] == 0:
-                # p rint(response.json()['results'])
                 with open(FILENAME_QUESTIONS, "w", encoding="UTF-8") as file:
                     self.questions = response.json()['results']
                     json.dump(self.questions, file)
@@ -172,14 +184,16 @@ class Quiz:
                     self.category_number = category_number
                     return category_number
             except ValueError:
-                    print("Wrong category. Please, try again!")
-                    input("Press any key to continue")
+                print("Wrong category. Please, try again!")
+                input("Press any key to continue")
 
 
 def main():
     # clear screen
     clear_console()
     print("Hello and welcome to our online quiz consisting of 10 questions!")
+    score = Score()
+    score.name = input("Your name: ")
     quiz = Quiz()
     category = quiz.get_category_from_file(FILENAME_CATEGORY)
     if category:
@@ -188,7 +202,28 @@ def main():
         question_json = quiz.get_questions()
         question = Question()
         question.create_questions(question_json)
-        question.print_questions()
+        for i in range(len(question.question)):
+            current_question = question.print_question(i)
+            while True:
+                answer = input("Your answer: ")
+                try:
+                    number_answer = int(answer)
+                    if question.is_correct_answer(
+                        current_question,
+                        number_answer - 1
+                        ):
+                        score.point_add(10)
+                        print("Correct answer!")
+                        print(f"Your points: {score.point}")
+
+                    else:
+                        print("Wrong answer!")
+                    break
+                except ValueError:
+                    print("Please, choose an integer digit!")
+                    input("Press any key to continue")
+
+        print(f"Congratulations! Your points: {score.point}")
 
     else:
         print("Something went wrong. No file with categories.")
